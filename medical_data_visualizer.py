03c51853f13f0ebcc46744ed859245a3774bd701
Doreen970 <doreenwanyama20@gmail.com>
2024-04-05 00:55:56 +0300

    grouped data by cardio and created a cat plot

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -20,28 +20,30 @@ df['gluc'] = np.where(df['gluc'] > 1, 1, 0)
 
 # Draw Categorical Plot
 def draw_cat_plot():
-    # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-   
+    # Create DataFrame for cat plot using pd.melt using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
     df_cat = df[['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight']]
-    df_cat = pd.melt(df_cat)
+    print(df_cat.head())
+    df_cat = pd.melt(df_cat, var_name='variable', value_name='value')
 
+    # Include the 'cardio' column from the original DataFrame 'df'
+    df_cat['cardio'] = df['cardio']
 
-    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
-    
+    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature.
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='count')
+    print(df_cat.head(10))
 
     # Draw the catplot with 'sns.catplot()'
-
-
+    g = sns.catplot(x='variable', y='count', hue='value', col='cardio', data=df_cat, kind='bar')
 
     # Get the figure for the output
-    fig = None
+    fig = g.fig
 
-
-    # Do not modify the next two lines
+    # Save the figure
     fig.savefig('catplot.png')
+
     return fig
 
+draw_cat_plot()
 
 # Draw Heat Map
 def draw_heat_map():
